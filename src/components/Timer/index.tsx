8eeab9dd50bdd0d8e49a27ce8dee5f62b4b93525
Gregory Olive <gregoryolive@outlook.com>
2022-07-21 02:39:58 -0300

    Build out basic count up timer

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,26 +1,52 @@
-import { ReactElement } from 'react';
+import { useState, useEffect, ReactElement } from 'react';
 
-const Counter = (): ReactElement => {
+interface CounterProps {
+  time: number,
+}
+
+const Counter = ({ time }: CounterProps): ReactElement => {
+  const format = (time: number, div: number): string => {
+    const str = Math.floor((time / div) % 60).toString();
+    return (str.length > 1) ? str : '0' + str;
+  };
+
   return (
-    <div className='col-span-2'>
-      <span className='text-8xl sm:text-9xl'>
-        03:10
-      </span>
+    <div className='text-8xl sm:text-9xl flex justify-center col-span-2'>
+      <span className='w-32 sm:w-40 text-end'>{format(time, 60000)}</span>
+      <span>:</span>
+      <span className='w-32 sm:w-40'>{format(time, 1000)}</span>
     </div>
   );
 };
 
 const Timer = (): ReactElement => {
+  const [time, setTime] = useState(0);
+  const [play, setPlay] = useState(false);
+  
+  const toggleTimer = (): void => setPlay(!play);
+
+  useEffect(() => {
+    const tick = setTimeout(() => {
+      setTime(time + 1000);
+    }, 1000);
+
+    return () => clearTimeout(tick);
+  });
+
   return (
     <div className='grid grid-cols-2 gap-x-3 gap-y-5'>
-      <Counter />
+      <Counter time={time} />
 
       <button 
+        onClick={toggleTimer}
         className='timer-btn bg-teal-500 text-neutral-50 dark:text-neutral-900'
       >
         Start
       </button>
-      <button className='timer-btn bg-gray-300 dark:bg-slate-700'>
+
+      <button
+        className='timer-btn bg-gray-300 dark:bg-slate-700'
+      >
         Stop
       </button>
     </div>
