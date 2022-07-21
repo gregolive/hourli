ca3fb02f01590435bf6fa13d67e76e7a8c949c85
Gregory Olive <gregoryolive@outlook.com>
2022-07-21 14:34:56 -0300

    Add break and clock in/out functionality to timer

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,54 +1,88 @@
 import { useState, useEffect, ReactElement } from 'react';
+import { BiCoffeeTogo } from 'react-icons/bi';
 
-interface CounterProps {
+interface SubComponentProps {
   time: number,
-}
+};
 
-const Counter = ({ time }: CounterProps): ReactElement => {
-  const format = (time: number, div: number): string => {
-    const str = Math.floor((time / div) % 60).toString();
-    return (str.length > 1) ? str : '0' + str;
-  };
+const formatClock = (time: number, div: number): string => {
+  const str = Math.floor((time / div) % 60).toString();
+  return (str.length > 1) ? str : '0' + str;
+};
 
+const Counter = ({ time }: SubComponentProps): ReactElement => {
   return (
     <div className='text-8xl sm:text-9xl flex justify-center col-span-2'>
-      <span className='w-32 sm:w-40 text-end'>{format(time, 60000)}</span>
+      <span className='w-32 sm:w-40 text-end'>{formatClock(time, 3600000)}</span>
       <span>:</span>
-      <span className='w-32 sm:w-40'>{format(time, 1000)}</span>
+      <span className='w-32 sm:w-40'>{formatClock(time, 60000)}</span>
     </div>
+  );
+};
+
+const BreakInfo = ({ time }: SubComponentProps): ReactElement => {
+  return (
+    <>
+      <BiCoffeeTogo />
+      <span>Break time</span>
+      <span className='w-14'>
+        {formatClock(time, 3600000)}:{formatClock(time, 60000)}
+      </span>
+    </>
   );
 };
 
 const Timer = (): ReactElement => {
   const [time, setTime] = useState(0);
-  const [play, setPlay] = useState(false);
+  const [timing, setTiming] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [breakTime, setBreakTime] = useState(0);
   
-  const toggleTimer = (): void => setPlay(!play);
+  const toggleTimer = (): void => setTiming(!timing);
+  const clockInOut = (): void => {
+    setWorking(!working);
+    toggleTimer();
+  };
 
   useEffect(() => {
-    const tick = setTimeout(() => {
+    let tick = setTimeout(() => {
       setTime(time + 1000);
     }, 1000);
+    if (!timing) clearInterval(tick);
 
     return () => clearTimeout(tick);
-  });
+  }, [time, timing]);
+
+  useEffect(() => {
+    let tock = setTimeout(() => {
+      setBreakTime(breakTime + 1000);
+    }, 1000);
+    if (working && timing) clearInterval(tock);
+
+    return () => clearTimeout(tock);
+  }, [breakTime, timing, working]);
 
   return (
     <div className='grid grid-cols-2 gap-x-3 gap-y-5'>
       <Counter time={time} />
 
       <button 
-        onClick={toggleTimer}
-        className='timer-btn bg-teal-500 text-neutral-50 dark:text-neutral-900'
+        onClick={clockInOut}
+        className='timer-btn bg-teal-500 text-neutral-50 dark:text-neutral-900 drop-shadow-md'
       >
-        Start
+        {(working) ? 'Clock out' : 'Clock in'}
       </button>
 
       <button
-        className='timer-btn bg-gray-300 dark:bg-slate-700'
+        onClick={toggleTimer}
+        className='timer-btn bg-gray-300 dark:bg-cyan-900 drop-shadow'
       >
-        Stop
+        {(working && !timing) ? 'Off Break' : 'Break'}
       </button>
+
+      <div className='text-xl flex items-center justify-center col-span-2 gap-1 h-7'>
+        {(working && !timing) && <BreakInfo time={breakTime} />}
+      </div>
     </div>
   );
 };
